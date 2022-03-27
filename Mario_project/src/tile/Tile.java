a6e3939b6feb7711f5ae0fecdcb1af06de6d0a74
velovan <velovan@users.noreply.github.com>
2022-03-27 18:36:05 +0300

    Update Tile.java

--- Mario_project/src/tile/Tile.java
+++ Mario_project/src/tile/Tile.java
@@ -8,17 +8,16 @@ public class Tile {
 	private BufferedImage image;
 	private boolean blocked;
 
-	public Tile(BufferedImage image, boolean blocked) {
+	public Tile(BufferedImage image, boolean blocked){
 	    this.image = image;
 	    this.blocked = blocked;
 	}
 
-	public BufferedImage getImage() {
+	public BufferedImage getImage(){
            return image;
 	}
 
-	public boolean isBlocked() {
+	public boolean isBlocked(){
 	   return blocked;
 	}
-
 }
