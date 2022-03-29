05d5ca1f30698b1bdf9692583d75e21f7a3e4ff3
velovan <velovan@users.noreply.github.com>
2022-03-29 23:16:00 +0300

    Update Game.java

--- Mario_project/src/Game.java
+++ Mario_project/src/Game.java
@@ -3,15 +3,12 @@ import javax.swing.JFrame;
 public class Game {
 
 	public static void main(String [] args){
-		
-		JFrame frame = new JFrame("rrr");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.setContentPane(new GamePanel());
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-		
+	     JFrame frame = new JFrame("rrr");
+	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     frame.setResizable(false);
+	     frame.setContentPane(new GamePanel());
+	     frame.pack();
+	     frame.setLocationRelativeTo(null);
+	     frame.setVisible(true);	
 	}
-	
 }
